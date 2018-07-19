cb6755d7b03e51fac70fdd36d8c78c888e2dcc00
Jamie.Brown <jamiebrown201@hotmail.com>
2018-07-19 17:04:48 +0100

    api-limi-module

---
+++ bin/lib/api-limit-check.js
@@ -0,0 +1,135 @@
+const AWS = require('aws-sdk');
+
+const limitTable = process.env.LIMIT_TABLE;
+const awsRegion = process.env.AWS_REGION;
+
+const apiLimits = {
+	deepl: process.env.DEEPL_API_LIMIT,
+	google: process.env.GOOGLE_API_LIMIT
+};
+
+const database = new AWS.DynamoDB.DocumentClient({ region: awsRegion });
+
+function monthCreate({ month, year }) {
+	return new Promise((resolve, reject) => {
+		const params = {
+			TableName: limitTable,
+			Item: {
+				year,
+				month,
+				createdAt: Date.now(),
+				updatedAt: Date.now()
+			}
+		};
+
+		Object.keys(apiLimits).forEach(provider => {
+			params.Item[provider] = 0;
+		});
+
+		database.put(params, (error, result) => {
+			if (error) {
+				reject(error);
+			}
+			resolve(result);
+		});
+	});
+}
+
+function withinApiLimit({ provider }) {
+	return new Promise((resolve, reject) => {
+		if (!apiLimits[provider]) {
+			reject(new Error('API provider does not exist'));
+		}
+
+		const month = new Date().getMonth();
+		const year = new Date().getYear();
+
+		const params = {
+			TableName: limitTable,
+			Key: {
+				month,
+				year
+			}
+		};
+
+		database.get(params, async (error, result) => {
+			if (error) {
+				reject(error);
+				return;
+			}
+
+			if (Object.keys(result).length === 0) {
+				try {
+					await monthCreate({ month, year });
+					resolve(true);
+				} catch (error) {
+					reject(error);
+				}
+				return;
+			}
+
+			result.Item[provider] >= apiLimits[provider]
+				? resolve(false)
+				: resolve(true);
+		});
+	});
+}
+
+function updateApiLimitUsed({ articleCharacters, provider }) {
+	return new Promise((resolve, reject) => {
+		if (!apiLimits[provider]) {
+			reject(new Error('API provider does not exist'));
+			return;
+		}
+
+		const month = new Date().getMonth();
+		const year = new Date().getYear();
+
+		const params = {
+			TableName: limitTable,
+			Key: {
+				month,
+				year
+			},
+			ExpressionAttributeValues: {
+				':articleCharacters': articleCharacters,
+				':updatedAt': Date.now()
+			},
+			UpdateExpression: `SET updatedAt = :updatedAt ADD ${provider} :articleCharacters`,
+			ReturnValues: 'ALL_NEW'
+		};
+
+		database.update(params, (error, result) => {
+			if (error) {
+				reject(error);
+			}
+			resolve(result);
+		});
+	});
+}
+
+(async () => {
+	try {
+		const data = await updateApiLimitUsed({
+			articleCharacters: 50,
+			provider: 'google'
+		});
+		console.log(data);
+	} catch (error) {
+		console.log(error);
+	}
+})();
+
+(async () => {
+	try {
+		const data = await withinApiLimit({ provider: 'google' });
+		console.log('resolved', data);
+	} catch (error) {
+		console.log('rejected', error);
+	}
+})();
+
+module.exports = {
+	withinApiLimit,
+	updateApiLimitUsed
+};
