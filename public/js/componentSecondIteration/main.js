76561f059af1e46931971ea630ca05ff1c32d96c
Lily2point0 <lily.madar@ft.com>
2019-07-08 13:44:27 +0100

    general cleanup

--- public/js/componentSecondIteration/main.js
+++ public/js/componentSecondIteration/main.js
@@ -4,19 +4,10 @@ var toggleButton = document.querySelector('.ftlabs-translation__toggle');
 var articleTitle = document.querySelector('h1.topper__headline span:first-child');
 var articleStandfirst = document.querySelector('.topper__standfirst');
 var articleText = document.querySelectorAll('.article__content-body > p');
-var translationLoading = document.querySelector('.ftlabs-translation--loading');
-var translationCollapsed = document.querySelector(
-	'.ftlabs-translation__accordion--collapsed'
-);
-var translationExpanded = document.querySelector(
-	'.ftlabs-translation__accordion--expanded'
-);
+
 var turnOffButton = document.querySelector('.ftlabs-translation__button--off');
-var translateAll = document.querySelector(
-	'.ftlabs-translation__tickbox input[type="checkbox"]'
-);
-var translationPrompt = document.querySelector('.ftlabs-translation__prompt');
-var accordionButton = document.querySelector('.ftlabs-translation__toggle');
+var translateAll = document.querySelector('.ftlabs-translation__tickbox input[type="checkbox"]');
+
 var translationError = document.querySelector('.ftlabs-translation--error');
 var disclaimer = document.querySelector('.ftlabs-translation__disclaimer');
 var articleId = document
@@ -42,7 +33,6 @@ function init(langs) {
 		elementDiv.classList.add('o-forms__group');
 		elementDiv.classList.add('o-forms__group--inline');
 		elementDiv.classList.add('ftlabs-translation__language-selection-element');
-		// elementDiv.classList.add('ftlabs-translation__language-selection-' + langs[i].name.toLowerCase());
 		elementDiv.setAttribute('data-lang-code', langs[i].code);
 
 		listItem.value = langs[i].code;
@@ -101,20 +91,6 @@ function init(langs) {
 
 function greyOutOtherElements() {}
 
-function toggleTranslationAccordion() {
-	translationExpanded.classList.toggle('ftlabs-translation--hidden');
-	accordionButton.innerText = getAccordionCTA();
-
-	var state = translationExpanded.classList.contains(
-		'ftlabs-translation--hidden'
-	)
-		? 'collapsed'
-		: 'expanded';
-	logComponentInteractions(`translation-${state}`, languageSelect.value);
-
-	accordionButton.classList.toggle('o-buttons-icon--arrow-down');
-	accordionButton.classList.toggle('o-buttons-icon--arrow-up');
-}
 
 function toggleTranslateAll(e) {
 	if (e.currentTarget.checked) {
@@ -200,9 +176,6 @@ function showTranslation(language) {
 }
 
 function unsuccessfulTranslationRequest(err) {
-	// translationCollapsed.classList.remove('ftlabs-translation--blur');
-	// translationExpanded.classList.remove('ftlabs-translation--blur');
-	// translationLoading.classList.add('ftlabs-translation--hidden');
 	translationError.classList.remove('ftlabs-translation--hidden');
 	logComponentInteractions('translation-error', languageSelect.value, err);
 }
@@ -244,14 +217,6 @@ function successfulTranslationRequest(selector, language) {
 	});
 
 	showSplitView();
-
-	// translationCollapsed.classList.remove('ftlabs-translation--blur');
-	// translationExpanded.classList.remove('ftlabs-translation--blur');
-	// translationLoading.classList.add('ftlabs-translation--hidden');
-	// turnOffButton.classList.remove('ftlabs-translation--hidden');
-
-	// translationPrompt.innerHTML =
-	// 	'This article has been translated into ' + language;
 }
 
 function removeTranslation() {
@@ -274,9 +239,6 @@ function removeTranslation() {
 
 	turnOffButton.classList.add('ftlabs-translation--hidden');
 	languageSelect.selectedIndex = 0;
-	translationPrompt.innerHTML =
-		'Automated translations are now available for <a href="https://www.ft.com/brexit" class="n-content-tag">Brexit</a> and <a href="https://www.ft.com/companies/technology" class="n-content-tag">Technology</a> articles on the FT';
-	accordionButton.innerText = getAccordionCTA();
 }
 
 function logComponentInteractions(interaction, language = 'EN', error = null) {
@@ -294,24 +256,6 @@ function logComponentInteractions(interaction, language = 'EN', error = null) {
 	// );
 }
 
-function getAccordionCTA() {
-	var cta;
-
-	if (translationExpanded.classList.contains('ftlabs-translation--hidden')) {
-		if (languageSelect.selectedIndex !== 0) {
-			cta = 'Change';
-			disclaimer.classList.remove('ftlabs-translation--hidden');
-		} else {
-			cta = 'Try it';
-			disclaimer.classList.add('ftlabs-translation--hidden');
-		}
-	} else {
-		cta = 'Hide';
-		disclaimer.classList.remove('ftlabs-translation--hidden');
-	}
-
-	return cta;
-}
 
 function getTranslationData(e) {
 	fetch(`https://ftlabs-left.herokuapp.com/check/${articleId}/${pubDate}`)
